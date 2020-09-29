d5c95edea210a747c1d4159e7a90820c3c7e735b
J. Bromley <jay@ai.co>
2020-09-28 18:16:29 -0700

    Updated for Processing 3.

--- PerlinTest/src/org/jbromley/perlintest/PerlinTest.java
+++ PerlinTest/src/org/jbromley/perlintest/PerlinTest.java
@@ -9,10 +9,15 @@ import peasy.*;
 
 public class PerlinTest extends PApplet {
 
-    private static final int SIZE = 320;
+    private static final int SIZE = 640;
 
     private PeasyCam camera;
     private float noiseScale = 0.02f;
+
+    public void settings()
+    {
+        size(SIZE, SIZE, "processing.opengl.PGraphics3D");
+    }
 
     public void setup() {
         size(SIZE, SIZE, P3D);
@@ -21,7 +26,7 @@ public class PerlinTest extends PApplet {
         camera.setMinimumDistance(0);
         camera.setMaximumDistance(512);
 
-	colorMode(HSB, 255);
+        colorMode(HSB, 255);
     }
 
     public void draw() {
@@ -30,7 +35,7 @@ public class PerlinTest extends PApplet {
             for (int y = -SIZE / 2; y < SIZE / 2; ++y) {
                 float noiseVal = noise((mouseX + x) * noiseScale, (mouseY + y) *noiseScale);
                 stroke(noiseVal * 255, 255.0f, 255.0f);
-                // line(x, mouseY+noiseVal*80, x, height);
+                // line(x, mouseY + noiseVal * 80, x, height);
                 point(x, y, -128 + noiseVal * 255);
             }
         }
