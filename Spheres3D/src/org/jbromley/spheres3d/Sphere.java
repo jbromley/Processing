e3e34441b25c5224e28b6ad0353c5161142d4834
J. Bromley <jay@ai.co>
2020-09-28 18:11:10 -0700

    Updated Spheres3D for Processing 3.
    Also cleaned up tabs and spaces in code.

--- Spheres3D/src/org/jbromley/spheres3d/Sphere.java
+++ Spheres3D/src/org/jbromley/spheres3d/Sphere.java
@@ -13,20 +13,19 @@ public class Sphere {
     private int color;
     
     public Sphere(PVector initPos, float radius, PApplet applet) {
-	p = applet;
-	pos = initPos;
-	this.radius = radius;
-	color = p.color(p.random(0, 255), p.random(0, 255), p.random(0, 255), 
-			p.random(0, 255));
+        p = applet;
+        pos = initPos;
+        this.radius = radius;
+        color = p.color(p.random(0, 255), p.random(0, 255), p.random(0, 255), p.random(0, 255));
     }
       
     public void draw() {
-	p.pushMatrix();
-	p.noStroke();
-	p.fill(color);
-	p.translate(pos.x, pos.y, pos.z);
-	p.sphere(radius);
-	p.popMatrix();
+        p.pushMatrix();
+        p.noStroke();
+        p.fill(color);
+        p.translate(pos.x, pos.y, pos.z);
+        p.sphere(radius);
+        p.popMatrix();
     }
 }
 
--- Spheres3D/src/org/jbromley/spheres3d/Spheres3D.java
+++ Spheres3D/src/org/jbromley/spheres3d/Spheres3D.java
@@ -12,40 +12,43 @@ public class Spheres3D extends PApplet {
 
     private List<Sphere> spheres;
 	  
+    public void settings() {
+        size(1280, 800, "processing.opengl.PGraphics3D");
+    }
+
     public void setup() {
-	size(1280, 800, P3D); 
-	background(0);
-	lights();
-	  	  
-	spheres = new ArrayList<Sphere>();
-	for (int i = 0; i < NUM_SPHERES; ++i) {
-	    float xLimit = width / 2;
-	    float yLimit = height / 2;
-	    PVector pos = new PVector(random(-xLimit, xLimit), 
-				      random(-yLimit, yLimit), 
-				      random(-yLimit, yLimit));
-	    float radius = 8;
-	    Sphere sphere = new Sphere(pos, radius, this);
-	    spheres.add(sphere);
-	}
+        background(0);
+        lights();
+              
+        spheres = new ArrayList<Sphere>();
+        for (int i = 0; i < NUM_SPHERES; ++i) {
+            float xLimit = width / 2;
+            float yLimit = height / 2;
+            PVector pos = new PVector(random(-xLimit, xLimit), 
+                          random(-yLimit, yLimit), 
+                          random(-yLimit, yLimit));
+            float radius = 8;
+            Sphere sphere = new Sphere(pos, radius, this);
+            spheres.add(sphere);
+        }
     }
 
     public void draw() {
-	background(0);
-	lights();
-	pushMatrix();
-	//scale(2);
-	translate(width / 2, height / 2, 0);
-	rotateX(mouseY * 0.005f);
-	rotateY(mouseX * 0.005f);
-	for (Sphere sphere : spheres) {
-	    sphere.draw();
-	}
-	popMatrix();
+        background(0);
+        lights();
+        pushMatrix();
+        //scale(2);
+        translate(width / 2, height / 2, 0);
+        rotateX(mouseY * 0.005f);
+        rotateY(mouseX * 0.005f);
+        for (Sphere sphere : spheres) {
+            sphere.draw();
+        }
+        popMatrix();
     }
 
     public static void main(String args[]) {
-	PApplet.main(new String[] {"org.jbromley.spheres3d.Spheres3D"});
+        PApplet.main(new String[] {"org.jbromley.spheres3d.Spheres3D"});
     }
 
 }
