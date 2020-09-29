5aaf1c53a564d22cc55e1945162ccd0b00b3c108
J. Bromley <jay@ai.co>
2020-09-28 18:04:14 -0700

    Updated ColorTiles for Processing 3.

--- ColorTiles/src/org/jbromley/colortiles/ColorTiles.java
+++ ColorTiles/src/org/jbromley/colortiles/ColorTiles.java
@@ -15,43 +15,47 @@ public class ColorTiles extends PApplet {
     private PeasyCam camera;
     private ArrayList<Tile> tiles;
 
+    public void settings()
+    {
+        size(1600, 1600, "processing.opengl.PGraphics3D");
+    }
+
     public void setup() {
-	frame.setTitle("Color Tiles");
-	size(1000, 1000, P3D);
-	colorMode(HSB, 1.0f);
-  
-	camera = new PeasyCam(this, 600);
-	camera.setMinimumDistance(0);
-	camera.setMaximumDistance(1600);
-  
-	// Set up the tiles.
-	tiles = new ArrayList<Tile>();
-	for (int x = -HALF_WIDTH; x <= HALF_WIDTH; ++x) {
-	    for (int y = -HALF_WIDTH; y <= HALF_WIDTH; ++y) {
-		for (int z = -HALF_WIDTH; z <= HALF_WIDTH; ++z) {
-		    int xpos = 3 * x * TILE_WIDTH / 2;
-		    int ypos = 3 * y * TILE_HEIGHT / 2;
-		    int zpos = z * 4 * TILE_WIDTH / 2;
-		    Tile tile = new Tile(xpos, ypos, zpos, TILE_WIDTH, 
-					 TILE_HEIGHT, TILE_DEPTH, 0.001f, 
-					 this);
-		    tiles.add(tile);
-		}
-	    }
-	}
+        surface.setTitle("Color Tiles");
+        colorMode(HSB, 1.0f);
+      
+        camera = new PeasyCam(this, 600);
+        camera.setMinimumDistance(0);
+        camera.setMaximumDistance(1600);
+      
+        // Set up the tiles.
+        tiles = new ArrayList<Tile>();
+        for (int x = -HALF_WIDTH; x <= HALF_WIDTH; ++x) {
+            for (int y = -HALF_WIDTH; y <= HALF_WIDTH; ++y) {
+            for (int z = -HALF_WIDTH; z <= HALF_WIDTH; ++z) {
+                int xpos = 3 * x * TILE_WIDTH / 2;
+                int ypos = 3 * y * TILE_HEIGHT / 2;
+                int zpos = z * 4 * TILE_WIDTH / 2;
+                Tile tile = new Tile(xpos, ypos, zpos, TILE_WIDTH, 
+                         TILE_HEIGHT, TILE_DEPTH, 0.001f, 
+                         this);
+                tiles.add(tile);
+            }
+            }
+        }
     }
 
     public void draw() {
-	background(0);
-	for (int i = 0; i < tiles.size(); ++i) {
-	    Tile tile = tiles.get(i);
-	    tile.update();
-	    tile.draw();
-	}
+        background(0);
+        for (int i = 0; i < tiles.size(); ++i) {
+            Tile tile = tiles.get(i);
+            tile.update();
+            tile.draw();
+        }
     }
 
     public static void main(String args[]) {
-	PApplet.main(new String[] {"org.jbromley.colortiles.ColorTiles"});
+        PApplet.main(new String[] {"org.jbromley.colortiles.ColorTiles"});
     }
 }
 
